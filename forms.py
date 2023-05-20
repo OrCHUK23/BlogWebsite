ff459a5e4d9fe400d5c98974fa90c3a9a507e904
Or <henor1997@gmail.com>
2023-05-20 13:38:49 +0300

    Added new login form.

--- forms.py
+++ forms.py
@@ -23,3 +23,4 @@ class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
+    forgot_password = SubmitField("Forgot Password")  # New button
